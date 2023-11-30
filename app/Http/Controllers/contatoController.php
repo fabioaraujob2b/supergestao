27b98a47371b9623acd7d49cedaeb2222f21efbc
fabioaraujob2b <fabioaraujob2b@gmail.com>
2023-11-30 17:05:45 -0300

    Ajustando o front-end

--- app/Http/Controllers/contatoController.php
+++ app/Http/Controllers/contatoController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 class contatoController extends Controller
 {
     public function contato(){
+        var_dump($_POST);
         return view('site.contato');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [indexController::class, 'index'])->name('site.index');
 Route::get('/sobrenos', [sobreNosController::class, 'sobreNos'])->name('site.sobrenos');
 Route::get('/contato', [contatoController::class, 'contato'])->name('site.contato');
+Route::post('/contato', [contatoController::class, 'contato'])->name('site.contato');
 Route::get('/login', function (){return 'Login';})->name('site.login');
 
 Route::prefix('app')->group(function() {
