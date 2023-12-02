82e197baefa95fa7d8ea8b9d23dd07c885ea07df
fabioaraujob2b <fabioaraujob2b@gmail.com>
2023-12-02 15:45:19 -0300

    Finalizando as migrations

--- database/migrations/2023_12_01_194156_create_unidades_table.php
+++ database/migrations/2023_12_01_194156_create_unidades_table.php
@@ -10,23 +10,23 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {
-        /* Schema::create('unidades', function (Blueprint $table) {
+    { 
+        Schema::create('unidades', function (Blueprint $table) {
             $table->id();
             $table->string('unidade', 5);
             $table->string('descricao', 30);
             $table->timestamps();
-        }); */
+        });
 
-        Schema::table('produtos', function(Blueprint $table){
+         Schema::table('produtos', function(Blueprint $table){
             $table->unsignedBigInteger('unidade_id');
             $table->foreign('unidade_id')->references('id')->on('unidades');
         });
 
-        Schema::table('produto_detalhes', function(Blueprint $table){
+        Schema::table('produtos_detalhes', function(Blueprint $table){
             $table->unsignedBigInteger('unidade_id');
             $table->foreign('unidade_id')->references('id')->on('unidades');
-        });
+        }); 
     }
 
     /**
@@ -34,7 +34,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-       Schema::table('produtos_detalhes', function(Blueprint $table){
+        Schema::table('produtos_detalhes', function(Blueprint $table){
             $table->dropForeign('produtos_detalhes_unidade_id_foreign');
             $$table->dropColumn('unidade_id');
         });
